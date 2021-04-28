6047762cb5cfebe794c06c61f0fb8387e97823c4
liyang <xxbarca@163.com>
2021-04-28 16:46:43 +0800

    使用Teleport将dialog放在body下面

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,21 +1,23 @@
 <template>
 	<template v-if="visible">
-		<div class="gulu-dialog-overlay" @click="onClickOverlay"></div>
-		<div class="gulu-dialog-wrapper">
-			<div class="mb">
-				<header>
-					<slot name="title"></slot>
-					<span @click="close" class="gulu-dialog-close"></span>
-				</header>
-				<main>
-					<slot name="content"></slot>
-				</main>
-				<footer>
-					<Button level="main" @click="ok">确定</Button>
-					<Button @click="cancel">取消</Button>
-				</footer>
+		<Teleport to="body">
+			<div class="gulu-dialog-overlay" @click="onClickOverlay"></div>
+			<div class="gulu-dialog-wrapper">
+				<div class="gulu-dialog">
+					<header>
+						<slot name="title" />
+						<span @click="close" class="gulu-dialog-close"></span>
+					</header>
+					<main>
+						<slot name="content" />
+					</main>
+					<footer>
+						<Button level="main" @click="ok">OK</Button>
+						<Button @click="cancel">Cancel</Button>
+					</footer>
+				</div>
 			</div>
-		</div>
+		</Teleport>
 	</template>
 
 </template>
