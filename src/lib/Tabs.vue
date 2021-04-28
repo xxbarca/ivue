888e6f6d0e98dd372dec4a3d2df17690ae95b784
liyang <xxbarca@163.com>
2021-04-28 19:56:56 +0800

    显示被选中的导航

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,7 +1,14 @@
 <template>
-	<div>
-		<div v-for="(t, index) in titles" :key="index">{{t}}</div>
-		<component v-for="(c, index) in defaults" :is="c" :key="index" />
+	<div class="gulu-tabs">
+		<div class="gulu-tabs-nav">
+			<span
+					:class="{selected: t === selected}"
+					class="gulu-tabs-nav-item"
+					v-for="(t, index) in titles" :key="index">{{t}}</span>
+		</div>
+		<div class="gulu-tabs-content">
+			<component class="gulu-tabs-content-item" v-for="(c, index) in defaults" :is="c" :key="index" />
+		</div>
 	</div>
 </template>
 
@@ -9,6 +16,12 @@
 	import Tab from "./Tab.vue"
 	export default {
 		name: "Tabs",
+		props: {
+			selected: {
+				type: String,
+				default: ''
+			}
+		},
 		setup(props, context) {
 			const defaults = context.slots.default()
 			defaults.forEach(tag => {
@@ -27,6 +40,39 @@
 	}
 </script>
 
-<style scoped>
-
+<style lang="scss">
+	$blue: #40a9ff;
+	$color: #333;
+	$border-color: #d9d9d9;
+	.gulu-tabs {
+		&-nav {
+			display: flex;
+			color: $color;
+			border-bottom: 1px solid $border-color;
+			position: relative;
+			&-item {
+				padding: 8px 0;
+				margin: 0 16px;
+				cursor: pointer;
+				&:first-child {
+					margin-left: 0;
+				}
+				&.selected {
+					color: $blue;
+				}
+			}
+			&-indicator {
+				position: absolute;
+				height: 3px;
+				background: $blue;
+				left: 0;
+				bottom: -1px;
+				width: 100px;
+				transition: all 250ms;
+			}
+		}
+		&-content {
+			padding: 8px 0;
+		}
+	}
 </style>
