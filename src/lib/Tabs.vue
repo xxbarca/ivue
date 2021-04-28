e7b69636b8749036e997b7a3b6216a231f915c25
liyang <xxbarca@163.com>
2021-04-28 19:21:47 +0800

    检查子组件类型

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -3,8 +3,19 @@
 </template>
 
 <script>
+	import Tab from "./Tab.vue"
 	export default {
-		name: "Tabs"
+		name: "Tabs",
+		setup(props, context) {
+			const defaults = context.slots.default()
+			defaults.forEach(tag => {
+				if (tag.type !== Tab) {
+					throw new Error("Tabs 子标签必须为Tab")
+				}
+			})
+
+
+		}
 	}
 </script>
 
