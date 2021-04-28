9a3ec37ca15ad35d1bdbac1e4eb8dbea8ff42b97
liyang <xxbarca@163.com>
2021-04-28 10:48:22 +0800

    dialog 添加 visible

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,13 +1,29 @@
 <template>
-	<Dialog />
+	<div>Dialog 示例</div>
+	<h1>示例1</h1>
+	<Button @click="toggle">toggle</Button>
+	<Dialog :visible="visible"/>
 </template>
 
 <script>
 	import Dialog from "../lib/Dialog.vue"
+	import Button from "../lib/Button.vue"
+	import {ref} from 'vue'
 	export default {
 		name: "DialogDemo",
 		components: {
-			Dialog
+			Dialog,
+			Button
+		},
+		setup() {
+			const visible = ref(false)
+			const toggle = () => {
+				visible.value = !visible.value
+			}
+			return {
+				toggle,
+				visible
+			}
 		}
 	}
 </script>
