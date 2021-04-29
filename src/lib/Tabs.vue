e93a5bbbdacbf816911dad472bb848883619f1e2
liyang <xxbarca@163.com>
2021-04-29 12:10:11 +0800

    动态设置 div 的位置

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,11 +1,16 @@
 <template>
 	<div class="gulu-tabs">
-		<div class="gulu-tabs-nav">
+		<div class="gulu-tabs-nav" ref="container">
 			<span
 					@click="select(t)"
 					:class="{selected: t === selected}"
 					class="gulu-tabs-nav-item"
-					v-for="(t, index) in titles" :key="index">{{t}}</span>
+					v-for="(t, index) in titles" :key="index"
+					:ref="el => { navItems[index] = el }"
+			>
+				{{t}}
+			</span>
+			<div ref="indicator" class="gulu-tabs-nav-indicator"></div>
 		</div>
 		<div class="gulu-tabs-content">
 			<component :key="current.props.title" class="gulu-tabs-content-item" :is="current"  />
@@ -15,7 +20,7 @@
 
 <script>
 	import Tab from "./Tab.vue"
-	import {computed} from 'vue'
+	import {computed, ref, onMounted, onUpdated} from 'vue'
 	export default {
 		name: "Tabs",
 		props: {
@@ -25,24 +30,48 @@
 			}
 		},
 		setup(props, context) {
+			const navItems = ref([])
+			const indicator = ref(null)
+			const container = ref(null)
 			const defaults = context.slots.default()
 			defaults.forEach(tag => {
 				if (tag.type !== Tab) {
 					throw new Error("Tabs 子标签必须为Tab")
 				}
 			})
+			const titles = defaults.map(tag => tag.props.title)
 			const current = computed(() => {
 				return defaults.find(tag => tag.props.title === props.selected)
 			})
-			const titles = defaults.map(tag => tag.props.title)
 			const select = (title) => {
 				context.emit('update:selected', title)
 			}
+			const x = () => {
+				const divs = navItems.value
+				const result = divs.filter(div => div.classList.contains('selected'))[0]
+				const {
+					width
+				} = result.getBoundingClientRect()
+				indicator.value.style.width = width + 'px'
+				const {left: left1} = container.value.getBoundingClientRect()
+				const {left: left2} = result.getBoundingClientRect()
+				const left = left2 - left1
+				indicator.value.style.left = left + 'px'
+			}
+			onMounted(() => {
+				x()
+			})
+			onUpdated(() => {
+				x()
+			})
 			return {
 				defaults,
 				titles,
 				current,
-				select
+				select,
+				navItems,
+				indicator,
+				container
 			}
 		}
 	}
